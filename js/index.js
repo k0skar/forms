64cc750126093d7dfba7dde17b389bf530d92cf3
k0skar <koskarnoza@gmail.com>
2020-01-23 06:08:46 +0200

    firefox fix

--- js/index.js
+++ js/index.js
@@ -77,6 +77,7 @@ class Dialog {
             this.domElement.show();
         } else {
             this.domElement.setAttribute('open')
+            this.domElement.setAttribute('display', 'block')
         }
 
     }
@@ -89,6 +90,7 @@ class Dialog {
             this.domElement.close();
         } else {
             this.domElement.removeAttribute('open')
+            this.domElement.setAttribute('display', 'none')
         }
     }
 }
